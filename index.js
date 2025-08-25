0f57dba95af5dee87b4ff328bbbe49db87ce502d
paleptonov <paleptonov@gmail.com>
2025-08-25 17:31:33 +0500

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+    output.innerHTML = 'Дружище ты лучший и у тебя всё получится!!!';
+});
